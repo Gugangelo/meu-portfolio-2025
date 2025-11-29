0653b4446b9260da8d25dd9195e9c74f734ac52a
Gustavo Angelo <78031691+Gugangelo@users.noreply.github.com>
2025-11-28 23:53:39 -0300

    feat: add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-700 p-1 text-center sm:text-base text-sm text-white">©2025 - Site desenvolvido por Gustavo Angelo</div>
+  )
+}
+
+export default Footer
